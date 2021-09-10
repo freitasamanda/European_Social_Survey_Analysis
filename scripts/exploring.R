276f5a41ba861c60d7db04a9e4a909915ed33cc9
Amanda Freitas <freitas.com6@gmail.com>
2021-09-10 15:04:07 -0300

    Initial script for exploratory data analysis

--- scripts/exploring.R
+++ scripts/exploring.R
@@ -5,10 +5,19 @@ library(tidyverse)
 library(ggplot2)
 library(readr)
 
-
 # Importing data sets -----------------------------------------------------
-clean_data <- read_csv("clean_data.csv")
-variables <- read_csv("variables.csv")
-news <- read_csv("news.csv")
-politics <- read_csv("politics.csv")
+data <- read_csv("working_data/clean_data_final.csv")
+
+original_variables <- read_csv("original_data/original_variables.csv")
+
+news <- read_csv("working_data/variables_news.csv")
+
+politics <- read_csv("working_data/variables_politics.csv")
+
+
+# Tasks -------------------------------------------------------------------
+#gerar gráficos da idade, country, edulevels, gender
+#gerar gráficos sobre as variáveis de notícias
+# gerar gráficos sobre as variáveis de política
+
 
